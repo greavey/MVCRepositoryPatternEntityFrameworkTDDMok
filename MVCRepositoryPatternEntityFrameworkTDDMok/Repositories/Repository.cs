995d9981729b369c0ad34db3ea1d25f0644ffc0c
Graham Reavey <graham.reavey@live.co.uk>
2018-11-06 20:07:43 +0000

    FirstCommit

---
+++ MVCRepositoryPatternEntityFrameworkTDDMok/Repositories/Repository.cs
@@ -0,0 +1,54 @@
+﻿using MVCRepositoryPatternEntityFrameworkTDDMok.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace MVCRepositoryPatternEntityFrameworkTDDMok.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<TEntity> enties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
